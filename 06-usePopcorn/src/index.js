7a54cfbde5d8a5aeb91ccbadb642768bf4e52949
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2024-01-30 22:39:54 +0545

    ongoing

--- 06-usePopcorn/src/index.js
+++ 06-usePopcorn/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating />
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+    {/* <App /> */}
   </React.StrictMode>
 );
