5612a9aeb573cae7cf5e12237c409a5bb6d4759c
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2024-01-31 23:11:15 +0545

    ongoing

--- 06-usePopcorn/src/index.js
+++ 06-usePopcorn/src/index.js
@@ -8,9 +8,11 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating />
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={("Terrible", "Bad", "Okay", "Good", "Amazing")}
+    />
+    <StarRating size={24} color="red" classname="test" defaultRating={3} />
     {/* <App /> */}
   </React.StrictMode>
 );
