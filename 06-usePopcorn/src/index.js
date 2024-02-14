595b146d698dbfc4540517460d2717a644b4a6a5
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2024-02-14 23:06:20 +0545

    used useEffect

--- 06-usePopcorn/src/index.js
+++ 06-usePopcorn/src/index.js
@@ -8,11 +8,11 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <App />
     {/* <StarRating
       maxRating={5}
       messages={("Terrible", "Bad", "Okay", "Good", "Amazing")}
     />
     <StarRating size={24} color="red" classname="test" defaultRating={3} /> */}
-    <App />
   </React.StrictMode>
 );
